a8266f1a7ee63c7aab6fda919276257e4d0f4d40
stormspiritLiu <a912883181@163.com>
2023-04-17 21:51:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
 nonebot.load_builtin_plugins('echo')
-
+nonebot.load_plugin("nonebot_plugin_bilibili_viode")
+# nonebot.load_plugin("nonebot_plugin_biliav")
 
 nonebot.load_from_toml("pyproject.toml")
 
